b40ae005388f73b3579d68febf05e75a255b3833
Ethemkk <you@example.com>
2024-11-20 20:06:19 +0300

    init

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="bg-[#7996c9]  relative flex justify-end py-5 w-2/3 justify-self-center gap-5">
+        <div className="text-black ">Home</div>
+        <div className="text-black ">project</div>
+        <div className="text-black mr-4">about me</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
